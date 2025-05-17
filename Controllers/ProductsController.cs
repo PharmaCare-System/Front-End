2b99530917099d559415a638e6643858dfc0dff8
mo3z10 <moazyasser070>
2025-05-18 02:49:57 +0300

    Feat: Adding

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using PharmaCare.Models.Categories;
+using PharmaCare.Models.Inventory;
 using PharmaCare.Models.Product;
 
 namespace PharmaCare.Controllers
@@ -17,32 +18,82 @@ namespace PharmaCare.Controllers
             _httpClient = httpClient;
             _env = env;
         }
-            
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
+
+        public async Task<IActionResult> Index(int? categoryId)
         {
-            var products = await _httpClient.GetFromJsonAsync<List<ProductReadVM>>("https://localhost:44350/api/Products");
+
+            var categories = await _httpClient.GetFromJsonAsync<List<CategoryReadVM>>("https://localhost:44350/api/Categories");
+            ViewBag.Categories = new SelectList(categories, "Id", "CategoryName", categoryId);
+
+            List<ProductReadVM> products;
+
+            if (categoryId.HasValue)
+            {
+                var CategoryProducts = await _httpClient.GetFromJsonAsync<CategoryProducts>($"https://localhost:44350/api/Categories/{categoryId}/Products");
+                products = CategoryProducts.Products.ToList();
+
+            }
+            else
+            {
+                products = await _httpClient.GetFromJsonAsync<List<ProductReadVM>>("https://localhost:44350/api/Products");
+            }
 
             return View(products);
         }
 
+
+
         [HttpGet]
-        public async Task<IActionResult> Create() { 
-        
+        public async Task<IActionResult> Create()
+        {
+
             var categories = await _httpClient.GetFromJsonAsync<List<CategoryReadVM>>("https://localhost:44350/api/Categories");
+            ViewBag.Categories = new SelectList(categories, "Id", "CategoryName");
             var inventories = await _httpClient.GetFromJsonAsync<List<PharmaCare.Models.Inventory.InventoryReadVM>>("https://localhost:44350/api/Inventory ");
             ViewBag.Inventories = new SelectList(inventories, "Id", "Name");
+
             return View();
 
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(ProductAddVM product)
+        public async Task<IActionResult> Create(ProductWithImage product)
         {
+            var categories = await _httpClient.GetFromJsonAsync<List<CategoryReadVM>>("https://localhost:44350/api/Categories");
+            ViewBag.Categories = new SelectList(categories, "Id", "CategoryName");
+            var inventories = await _httpClient.GetFromJsonAsync<List<PharmaCare.Models.Inventory.InventoryReadVM>>("https://localhost:44350/api/Inventory ");
+            ViewBag.Inventories = new SelectList(inventories, "Id", "Name");
+
             if (!ModelState.IsValid)
                 return View(product);
-            var response = await _httpClient.PostAsJsonAsync("https://localhost:44350/api/Products", product);
+            if (product.Image != null)
+            {
+                string imageroot = _env.WebRootPath;
+                string imagename = product.Image.FileName;
+                string imagepath = Path.Combine(imageroot, "Images", "Products", imagename);
+                using (var stream = System.IO.File.Create(imagepath))
+                {
+                    await product.Image.CopyToAsync(stream);
+                }
+                product.ImageURL = Path.Combine("/Images/Products", imagename).Replace("\\", "/");
+
+            }
+            var ProductModel = new ProductAddVM
+            {
+                Name = product.Name,
+                Price = product.Price,
+                QuantityInStock = product.QuantityInStock,
+                ExpiryDate = product.ExpiryDate,
+                BarCode = product.BarCode,
+                BulkAllowed = product.BulkAllowed,
+                PrescriptionRequired = product.PrescriptionRequired,
+                CategoryId = product.CategoryId,
+                ImageURL = product.ImageURL,
+                InventoryId = product.InventoryId
+
+            };
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:44350/api/Products", ProductModel);
             if (!response.IsSuccessStatusCode)
                 ModelState.AddModelError("", "Error creating product.");
             return RedirectToAction(nameof(Index));
@@ -59,7 +110,7 @@ namespace PharmaCare.Controllers
                 return View(product);
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return StatusCode(500, new
                 {
@@ -81,35 +132,53 @@ namespace PharmaCare.Controllers
 
             return RedirectToAction(nameof(Index));
         }
-
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
             var product = await _httpClient.GetFromJsonAsync<ProductReadVM>($"https://localhost:44350/api/Products/{id}");
-            var Inventories = await _httpClient.GetFromJsonAsync<List<PharmaCare.Models.Inventory.InventoryReadVM>>("https://localhost:44350/api/Inventory ");
-            ViewBag.Inventories = new SelectList(Inventories, "Id", "Name");
-            var UpdateModel = new ProductUpdateVM
+
+            var categories = await _httpClient.GetFromJsonAsync<List<CategoryReadVM>>("https://localhost:44350/api/Categories");
+            ViewBag.Categories = new SelectList(categories, "Id", "CategoryName");
+
+            var inventories = await _httpClient.GetFromJsonAsync<List<InventoryReadVM>>("https://localhost:44350/api/Inventory");
+            ViewBag.Inventories = new SelectList(inventories, "Id", "Name");
+            var productUpdate = new ProductUpdateVM
             {
                 Id = product.Id,
                 Name = product.Name,
                 Price = product.Price,
                 QuantityInStock = product.QuantityInStock,
-                ImageUrl = product.ImageUrl,
                 BulkAllowed = product.BulkAllowed,
                 PrescriptionRequired = product.PrescriptionRequired,
+                ImageURL = product.ImageUrl, 
                 InventoryId = product.InventoryId,
                 CategoryId = product.CategoryId
             };
-            return View(UpdateModel);
+
+            return View(productUpdate);
         }
+
         [HttpPost]
-        public async Task<IActionResult> Edit(ProductUpdateVM product)
+        public async Task<IActionResult> Edit(ProductUpdateVM model)
         {
+            var categories = await _httpClient.GetFromJsonAsync<List<CategoryReadVM>>("https://localhost:44350/api/Categories");
+            ViewBag.Categories = new SelectList(categories, "Id", "CategoryName");
+
+            var inventories = await _httpClient.GetFromJsonAsync<List<InventoryReadVM>>("https://localhost:44350/api/Inventory");
+            ViewBag.Inventories = new SelectList(inventories, "Id", "Name");
             if (!ModelState.IsValid)
-                return View(product);
-            var response = await _httpClient.PutAsJsonAsync($"https://localhost:44350/api/Products/{product.Id}", product);
+
+                return View(model);
+
+
+            var response = await _httpClient.PutAsJsonAsync($"https://localhost:44350/api/Products/{model.Id}", model);
+
             if (!response.IsSuccessStatusCode)
+            {
                 ModelState.AddModelError("", "Error updating product.");
+                return View(model);
+            }
+
             return RedirectToAction(nameof(Index));
         }
 
@@ -117,3 +186,4 @@ namespace PharmaCare.Controllers
 
     }
 }
+
